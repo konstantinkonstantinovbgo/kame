10c992ec1587613635d973050fc9c06b15a2f1e5
konstantin <keywebdesign@gmail.com>
2019-02-18 11:29:12 +0200

    add emails to config

--- config/emails.php
+++ config/emails.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'office@kameengineering.com' => 'Plamen Kamenarov',
-    'pkamenarov@yahoo.com'       => 'Plamen Kamenarov',
-    'keywebdesign@gmail.com'     => 'Plamen Kamenarov',
+    'plamen.kamenarov@kameengineering.com ' => 'Plamen Kamenarov',
+//    'office@kameengineering.com'            => 'Plamen Kamenarov',
+    'pkamenarov@yahoo.com'                  => 'Plamen Kamenarov',
+    'keywebdesign@gmail.com'                => 'Plamen Kamenarov',
 ];
