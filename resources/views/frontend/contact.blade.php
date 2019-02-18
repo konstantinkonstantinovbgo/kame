655dbb195133b1169a8a7cf77f73877267eb8cd4
konstantin <keywebdesign@gmail.com>
2019-02-18 11:31:03 +0200

    add emails to config

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -72,7 +72,7 @@
                                 <input type="text" name="name" id="name" class="form-control" placeholder="{{ trans('messages.name') }}" required>
                             </div>
                             <div class="col-lg-12">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="{{ trans('messages.contact-us') }}" required>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="{{ trans('messages.email-address') }}" required>
                             </div>
                             <div class="col-lg-12">
                                 <input type="text" name="subject" id="subject" class="form-control" placeholder="{{ trans('messages.subject') }}" required>
