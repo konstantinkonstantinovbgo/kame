44d6e420fc72a610764186290ca234d2d2e463fc
konstantin <keywebdesign@gmail.com>
2019-02-23 21:59:24 +0200

    check email

--- config/emails.php
+++ config/emails.php
@@ -2,7 +2,7 @@
 
 return [
     //    'plamen.kamenarov@kameengineering.com' => 'Plamen Kamenarov',
-    //    'office@kameengineering.com' => 'Plamen Kamenarov',
+    'office@kameengineering.com' => 'Plamen Kamenarov',
     //    'pkamenarov@yahoo.com'                  => 'Plamen Kamenarov',
     'keywebdesign@gmail.com'     => 'Plamen Kamenarov',
 ];
